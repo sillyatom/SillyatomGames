474056ce6768f1eb99df627def0ffe0bfddfde77
Sillyatom <sillyatom@gmail.com>
2016-09-17 21:19:47 +0530

    IAP Manager Update

--- Unity/TrainCardGame_iOS/Assets/Scripts/IAP/IAPManager.cs
+++ Unity/TrainCardGame_iOS/Assets/Scripts/IAP/IAPManager.cs
@@ -1,59 +1,249 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Purchasing;
 
-public class IAPManager : IStoreListener
+// Deriving the Purchaser class from IStoreListener enables it to receive messages from Unity Purchasing.
+public class IAPManager : MonoBehaviour, IStoreListener
 {
-    private IStoreController controller;
-    private IExtensionProvider extensions;
+    private static IStoreController m_StoreController;
+    // The Unity Purchasing system.
+    private static IExtensionProvider m_StoreExtensionProvider;
+    // The store-specific Purchasing subsystems.
 
-    public IAPManager()
+    // Product identifiers for all products capable of being purchased:
+    // "convenience" general identifiers for use with Purchasing, and their store-specific identifier
+    // counterparts for use with and outside of Unity Purchasing. Define store-specific identifiers
+    // also on each platform's publisher dashboard (iTunes Connect, Google Play Developer Console, etc.)
+
+    // General product identifiers for the consumable, non-consumable, and subscription products.
+    // Use these handles in the code to reference which product to purchase. Also use these values
+    // when defining the Product Identifiers on the store. Except, for illustration purposes, the
+    // kProductIDSubscription - it has custom Apple and Google identifiers. We declare their store-
+    // specific mapping to Unity Purchasing's AddProduct, below.
+    public static string kProductIDConsumable1000Coins = "1000Coins";
+    public static string kProductIDNonConsumable = "nonconsumable";
+    public static string kProductIDSubscription = "subscription";
+
+    void Start()
     {
-        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
-        builder.AddProduct("1000 Coins Pack", ProductType.Consumable, new IDs
-            {
-                { "com.sag.traincardgame.1000coins", MacAppStore.Name }
-            });
-        builder.AddProduct("No Ads", ProductType.NonConsumable, new IDs
-            {
-                { "com.sag.traincardgame.noads", MacAppStore.Name }
-            });
+        // If we haven't set up the Unity Purchasing reference
+        if (m_StoreController == null)
+        {
+            // Begin to configure our connection to Purchasing
+            InitializePurchasing();
+        }
+    }
 
+    public void InitializePurchasing()
+    {
+        // If we have already connected to Purchasing ...
+        if (IsInitialized())
+        {
+            // ... we are done here.
+            return;
+        }
+        /*
+        // Create a builder, first passing in a suite of Unity provided stores.
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        // Add a product to sell / restore by way of its identifier, associating the general identifier
+        // with its store-specific identifiers.
+        builder.AddProduct(kProductIDConsumable, ProductType.Consumable);
+        // Continue adding the non-consumable product.
+        builder.AddProduct(kProductIDNonConsumable, ProductType.NonConsumable);
+        // And finish adding the subscription product. Notice this uses store-specific IDs, illustrating
+        // if the Product ID was configured differently between Apple and Google stores. Also note that
+        // one uses the general kProductIDSubscription handle inside the game - the store-specific IDs 
+        // must only be referenced here. 
+        builder.AddProduct(kProductIDSubscription, ProductType.Subscription, new IDs()
+            {
+                { kProductNameAppleSubscription, AppleAppStore.Name },
+                { kProductNameGooglePlaySubscription, GooglePlay.Name },
+            });
+        */
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("1000Coins", ProductType.Consumable, new IDs
+            {       
+                { "com.sag.traincardgame.1000coins", AppleAppStore.Name }
+            });
+        builder.AddProduct("NoAds", ProductType.NonConsumable, new IDs
+            {
+                { "com.sag.traincardgame.noads", AppleAppStore.Name }
+            });
+        // Kick off the remainder of the set-up with an asynchrounous call, passing the configuration 
+        // and this class' instance. Expect a response either in OnInitialized or OnInitializeFailed.
         UnityPurchasing.Initialize(this, builder);
     }
 
-    /// <summary>
-    /// Called when Unity IAP is ready to make purchases.
-    /// </summary>
+
+    private bool IsInitialized()
+    {
+        // Only say we are initialized if both the Purchasing references are set.
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+
+    public void BuyConsumable()
+    {
+        // Buy the consumable product using its general identifier. Expect a response either 
+        // through ProcessPurchase or OnPurchaseFailed asynchronously.
+        BuyProductID("1000Coins");
+    }
+
+
+    public void BuyNonConsumable()
+    {
+        // Buy the non-consumable product using its general identifier. Expect a response either 
+        // through ProcessPurchase or OnPurchaseFailed asynchronously.
+        BuyProductID("No Ads");
+    }
+
+
+    public void BuySubscription()
+    {
+        // Buy the subscription product using its the general identifier. Expect a response either 
+        // through ProcessPurchase or OnPurchaseFailed asynchronously.
+        // Notice how we use the general product identifier in spite of this ID being mapped to
+        // custom store-specific identifiers above.
+        BuyProductID(kProductIDSubscription);
+    }
+
+
+    void BuyProductID(string productId)
+    {
+        // If Purchasing has been initialized ...
+        if (IsInitialized())
+        {
+            // ... look up the Product reference with the general product identifier and the Purchasing 
+            // system's products collection.
+            Product product = m_StoreController.products.WithID(productId);
+
+            // If the look up found a product for this device's store and that product is ready to be sold ... 
+            if (product != null && product.availableToPurchase)
+            {
+                BridgeDebugger.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                // ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed 
+                // asynchronously.
+                m_StoreController.InitiatePurchase(product);
+            }
+                // Otherwise ...
+                else
+            {
+                // ... report the product look-up failure situation  
+                BridgeDebugger.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+            // Otherwise ...
+            else
+        {
+            // ... report the fact Purchasing has not succeeded initializing yet. Consider waiting longer or 
+            // retrying initiailization.
+            BridgeDebugger.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+
+    // Restore purchases previously made by this customer. Some platforms automatically restore purchases, like Google.
+    // Apple currently requires explicit purchase restoration for IAP, conditionally displaying a password prompt.
+    public void RestorePurchases()
+    {
+        // If Purchasing has not yet been set up ...
+        if (!IsInitialized())
+        {
+            // ... report the situation and stop restoring. Consider either waiting longer, or retrying initialization.
+            BridgeDebugger.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        // If we are running on an Apple device ... 
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            // ... begin restoring purchases
+            BridgeDebugger.Log("RestorePurchases started ...");
+
+            // Fetch the Apple store-specific subsystem.
+            var apple = m_StoreExtensionProvider.GetExtension<IAppleExtensions>();
+            // Begin the asynchronous process of restoring purchases. Expect a confirmation response in 
+            // the Action<bool> below, and ProcessPurchase if there are previously purchased products to restore.
+            apple.RestoreTransactions((result) =>
+                {
+                    // The first phase of restoration. If no more responses are received on ProcessPurchase then 
+                    // no purchases are available to be restored.
+                    BridgeDebugger.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+                });
+        }
+            // Otherwise ...
+            else
+        {
+            // We are not running on an Apple device. No work is necessary to restore purchases.
+            BridgeDebugger.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+        }
+    }
+
+
+    //
+    // --- IStoreListener
+    //
+
     public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
     {
-        this.controller = controller;
-        this.extensions = extensions;
+        // Purchasing has succeeded initializing. Collect our Purchasing references.
+        BridgeDebugger.Log("OnInitialized: PASS");
+
+        // Overall Purchasing system, configured with products for this application.
+        m_StoreController = controller;
+        // Store specific subsystem, for accessing device-specific store features.
+        m_StoreExtensionProvider = extensions;
     }
 
-    /// <summary>
-    /// Called when Unity IAP encounters an unrecoverable initialization error.
-    ///
-    /// Note that this will not be called if Internet is unavailable; Unity IAP
-    /// will attempt initialization until it becomes available.
-    /// </summary>
+
     public void OnInitializeFailed(InitializationFailureReason error)
     {
+        // Purchasing set-up has not succeeded. Check error for reason. Consider sharing this reason with the user.
+        BridgeDebugger.Log("OnInitializeFailed InitializationFailureReason:" + error);
     }
 
-    /// <summary>
-    /// Called when a purchase completes.
-    ///
-    /// May be called at any time after OnInitialized().
-    /// </summary>
-    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
     {
+        // A consumable product has been purchased by this user.
+        if (String.Equals(args.purchasedProduct.definition.id, kProductIDConsumable1000Coins, StringComparison.Ordinal))
+        {
+            BridgeDebugger.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            // The consumable item has been successfully purchased, add 100 coins to the player's in-game score.
+//            ScoreManager.score += 100;
+        }
+            // Or ... a non-consumable product has been purchased by this user.
+            else if (String.Equals(args.purchasedProduct.definition.id, kProductIDNonConsumable, StringComparison.Ordinal))
+        {
+            BridgeDebugger.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            // TODO: The non-consumable item has been successfully purchased, grant this item to the player.
+        }
+            // Or ... a subscription product has been purchased by this user.
+            else if (String.Equals(args.purchasedProduct.definition.id, kProductIDSubscription, StringComparison.Ordinal))
+        {
+            BridgeDebugger.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            // TODO: The subscription item has been successfully purchased, grant this to the player.
+        }
+            // Or ... an unknown product has been purchased by this user. Fill in additional products here....
+            else
+        {
+            BridgeDebugger.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+        }
+
+        // Return a flag indicating whether this product has completely been received, or if the application needs 
+        // to be reminded of this purchase at next app launch. Use PurchaseProcessingResult.Pending when still 
+        // saving purchased products to the cloud, and when that save is delayed. 
         return PurchaseProcessingResult.Complete;
     }
 
-    /// <summary>
-    /// Called when a purchase fails.
-    /// </summary>
-    public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
     {
+        // A product purchase attempt did not succeed. Check failureReason for more detail. Consider sharing 
+        // this reason with the user to guide their troubleshooting actions.
+        BridgeDebugger.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
     }
 }
