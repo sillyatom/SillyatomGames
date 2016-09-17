39326f73d1efea859e758bfe4fd7db5f13e841b6
Sillyatom <sillyatom@gmail.com>
2016-09-17 21:26:58 +0530

    adding test socket projects

---
+++ Unity/UnityWebSocket/Assets/Example/EchoTest.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class EchoTest : MonoBehaviour
+{
+    public Text inputText;
+    public Text outputText;
+
+    private string _out = "";
+    private WebSocket _socket;
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        _socket = new WebSocket(new Uri("ws://52.35.235.159:8080/websocket/25"));
+        if (outputText != null)
+        {
+            outputText.text = "connecting...";
+        }
+        yield return StartCoroutine(_socket.Connect());
+
+        while (true)
+        {
+            string reply = _socket.RecvString();
+
+            if (reply != null)
+            {
+                _out = _out + reply + "\n";
+
+                if (outputText != null)
+                {
+                    outputText.text = _out;
+                }
+            }
+
+            if (_socket.error != null)
+            {
+                Debug.LogError("Error: " + _socket.error);
+                break;
+            }
+
+            yield return 0;
+        }
+        _socket.Close();
+    }
+
+    public void SendString()
+    {
+        _socket.SendString(inputText.text);
+    }
+}
